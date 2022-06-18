4effe597750207299c83278f71351fa8d6beb395
Igor Kharin <igkharin@gmail.com>
2022-06-18 23:17:48 +0300

    Finished 7th task

---
+++ 7.java
@@ -0,0 +1,23 @@
+1) swapArr - swapIntArr
+// уточнили тип данных для метода, меняющего местами два элемента в массиве
+2) leftRound - leftRoundArrIndex
+// метод "крутит" массив влево относительно заданного индекса
+3) turnAround - turnAroundArrBetween
+// метод разворачивает массив относительно заданных индексов
+4) TriagPer - calcTrianglePerimeter
+// вычисление периметра треугольника
+5) fileOpen - openFile
+6) maxFinder - findMax 
+// находит максимальное из чисел
+7) attackOfUnit - unit.attack()
+// вызов attack класса Unit
+8) func_deriv - calcDerivAtPoint
+// производная функции в точке
+9) rootEquation - findRootOfEquat
+// находит корень уравнения
+10) tvSwitcher - switchTvPower
+// вкл/выкл телевизор
+11) invertSquareSum - calcSumOfInvertSquares
+// вычисление суммы обратных квадратов заданных чисел
+12) GCD - findGCD
+// находит наибольший общий делитель
