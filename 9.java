eddd75acda62e433e0f432822f9bff2ac4795d7e
Igor Kharin <igkharin@gmail.com>
2022-06-24 23:05:10 +0400

    Finished 9th task

---
+++ 9.java
@@ -0,0 +1,12 @@
+1) double pi - final double PI
+2) int days = 7 - final int WEEK_DAYS = 7
+3) int age = 18 - final int MIN_AGE = 18
+4) String err = "Сообщение об ошибке" - final String ERR_MESSAGE = "Сообщение об ошибке"
+5) int value = 1000 - final int MAX_VALUE = 1000
+6) String city = "Moscow" - final String CAPITAL_CITY = "Moscow"
+7) double e - final double EXPONENT
+8) double inch = 2.54 - final double CM_PER_INCH = 2.54
+9) double speed = 120 - final double MAX_SPEED_KMPH = 90
+10) int limit = 10000 - final int MAX_TRANSFER_AMOUNT_USD = 10000
+11) int commision - final int MIN_COMMISION_PERCENTAGE
+12) int tax - finat int MAX_TAX_PERCANTAGE
